bd23b7f68cd4c2c2546d8b7c27af09e091a3944b
ankurgarg101 <akshay30nov@gmail.com>
2015-03-14 00:56:12 +0530

    Create application class to get app context throughout the app

---
+++ mobile/src/main/java/torch/sdslabs/co/in/torch/TorchApplication.java
@@ -0,0 +1,25 @@
+package torch.sdslabs.co.in.torch;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by marauder on 3/14/15.
+ */
+public class TorchApplication extends Application {
+
+    private static TorchApplication sInstance;
+
+    public static TorchApplication getInstance(){
+        return sInstance;
+    }
+
+    public static Context getAppContext(){
+        return sInstance.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
